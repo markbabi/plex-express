afbdab51958f9a97065fdd920613d4048e9a7fef
Mark Babirad <mbabirad@brainstation.io>
2024-03-25 11:16:22 -0700

    make Allyce happy

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const PORT = process.env.PORT || 5050;
 
 app.get("/", (req, res) => {
-  res.send(`<h1>Welcome to my Complex Express App</h1>`);
+  res.send(`<h1>Hello World</h1>`);
 });
 
 app.listen(PORT, () => {
